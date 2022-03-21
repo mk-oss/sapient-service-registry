626562d2809ffdcb3405e878fe9ed6ae05795978
MaruthaMuthu Kannan <maruthamuthu.kannan@gmail.com>
2022-03-21 10:59:25 +0530

    Add files via upload

---
+++ src/test/java/com/sapient/ticketing/sapientserviceregistry/SapientServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.ticketing.sapientserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapientServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
